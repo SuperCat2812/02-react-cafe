837587b24c3cfe8e0aec6cc5859f5fd3ada50614
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-05-23 12:59:27 +0200

    feat: implement voting functionality with reset option and notification for no feedback

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -1,14 +1,61 @@
 import CafeInfo from "../CafeInfo/CafeInfo";
-import VoteOptions from "../VoteOptions/VoteOptions";
-import VoteStats from "../VoteStats/VoteStats";
 import css from "./App.module.css";
+import { VoteType, type Votes } from "../../types/votes";
+import { useState } from "react";
+import Notification from "../Notification/Notifcation";
+import VoteStats from "../VoteStats/VoteStats";
+import VoteOptions from "../VoteOptions/VoteOptions";
 
 function App() {
+  const votes: Votes = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  const [counted, setCounted] = useState(votes);
+  const handleVote = (type: typeof VoteType) => {
+    setCounted({ ...counted, [type]: counted[type] + 1 });
+  };
+  const resetVotes = () => {
+    setCounted({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+  const totalVotes = counted.good + counted.neutral + counted.bad;
+  const positiveRate = totalVotes
+    ? Math.round((counted.good / totalVotes) * 100)
+    : 0;
+
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions />
-      <VoteStats />
+
+      {totalVotes > 0 ? (
+        <>
+          <VoteOptions
+            handleVote={handleVote}
+            resetVotes={resetVotes}
+            canReset={true}
+          />
+          <VoteStats
+            votes={counted}
+            positiveRate={positiveRate}
+            totalVotes={totalVotes}
+          />
+        </>
+      ) : (
+        <>
+          <VoteOptions
+            handleVote={handleVote}
+            resetVotes={resetVotes}
+            canReset={false}
+          />
+          <Notification />
+        </>
+      )}
     </div>
   );
 }
--- src/component/VoteOptions/VoteOptions.tsx
+++ src/component/VoteOptions/VoteOptions.tsx
@@ -1,11 +1,39 @@
+import type { VoteType } from "../../types/votes";
 import css from "./VoteOptions.module.css";
-export default function VoteOptions() {
+interface OptionProps {
+  handleVote: (key: typeof VoteType) => void;
+  resetVotes: () => void;
+  canReset: boolean;
+}
+export default function VoteOptions({
+  handleVote,
+  resetVotes,
+  canReset,
+}: OptionProps) {
   return (
     <div className={css.container}>
-      <button className={css.button}>Good</button>
-      <button className={css.button}>Neutral</button>
-      <button className={css.button}>Bad</button>
-      <button className={`${css.button} ${css.reset}`}>Reset</button>
+      <button
+        className={css.button}
+        onClick={() => handleVote("good")}>
+        Good
+      </button>
+      <button
+        className={css.button}
+        onClick={() => handleVote("neutral")}>
+        Neutral
+      </button>
+      <button
+        className={css.button}
+        onClick={() => handleVote("bad")}>
+        Bad
+      </button>
+      {canReset && (
+        <button
+          className={`${css.button} ${css.reset}`}
+          onClick={resetVotes}>
+          Reset
+        </button>
+      )}
     </div>
   );
 }
